7259e72cd8c1816544fede381152441ceb7d6f2c
idealflower-k <aekrcu@naver.com>
2024-01-18 15:20:59 +0900

    FY-73/feat: introduction swagger

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import (
     api_view,
     authentication_classes,
--- pong/settings.py
+++ pong/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django_filters",
     "authentication",
     "game",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
