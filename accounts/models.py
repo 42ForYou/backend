3b1c0318331325b51e127ba54f15a757f1e9b712
이동현 <earthicko@gmail.com>
2024-01-18 18:33:39 +0900

    FY-56/feat: add model "Profile" in "accounts" app (#6)
    - Add class "Profile"
    - Register "Profile" on admin page
    - Add migration script

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     intra_id = models.CharField(primary_key=True, max_length=32)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=32)
+    email = models.CharField(max_length=128)
+    # FIXME: "profile_pics" may cause directory path issues
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
