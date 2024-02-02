76f06fb47b6f7c027f9d15bf17c09222ca41cf77
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2024-02-02 13:36:17 +0900

    Fy 100/feature/profile api refactor (#22)
    * FY-100/fix: when get other user profile response data
    
    * FY-100/feat: save avator in local
    
    * FY-100/fix: remove dead code
    
    * FY-100/fix:  get Profile in retrieve
    
    * FY-100/fix: profile update

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -13,6 +13,10 @@ from rest_framework import status
 from rest_framework import permissions
 from drf_yasg.utils import swagger_auto_schema
 from pong.utils import CookieTokenAuthentication, CustomError
+import hashlib
+import os
+import shutil
+from django.core.files.storage import default_storage
 
 
 class IsOwner(permissions.BasePermission):
@@ -42,6 +46,7 @@ class ProfileViewSet(
         try:
             instance = self.get_object()
             if request.user.intra_id != kwargs["intra_id"]:
+                instance = Profile.objects.get(user=kwargs["intra_id"])
                 serializer = ProfileNotOwnerSerializer(instance)
             else:
                 serializer = ProfileSerializer(instance)
@@ -60,13 +65,45 @@ class ProfileViewSet(
         responses={200: WrapDataSwaggerOnlyProfileSerializer()},
     )
     def update(self, request, *args, **kwargs):
-        if "data" in request.data:
-            request.data = request.data["data"]
-        super().update(request, *args, **kwargs)
-        instance = self.get_object()
-        serializer = ProfileSerializer(instance)
-        return Response(
-            DataWrapperSerializer({"user": serializer.data}),
-            inner_serializer=ProfileResponseSerializer,
-            status=status.HTTP_200_OK,
-        )
+        try:
+            if request.content_type.startswith("image/"):
+                profile = request.user.profile
+                image_path = self.save_image(request, request.user.intra_id, profile)
+                profile.avator = image_path
+                profile.save()
+            else:
+                if "data" not in request.data:
+                    raise CustomError(
+                        exception="Invalid request",
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                    )
+                data = request.data.get("data")
+                instance = self.get_object()
+                serializer = self.get_serializer(instance, data=data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                self.perform_update(serializer)
+            instance = self.get_object()
+            serializer = ProfileSerializer(instance)
+            return Response(
+                DataWrapperSerializer(
+                    {"user": serializer.data, "match_history": [{}]},
+                    inner_serializer=ProfileResponseSerializer,
+                ).data,
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            raise CustomError(e, "Profile", status_code=status.HTTP_400_BAD_REQUEST)
+
+    def save_image(self, request, intra_id, profile):
+        hased_filename = hashlib.sha256(intra_id.encode()).hexdigest()
+        file_path = f"images/avator/{hased_filename}.jpeg"
+
+        with open(file_path, "wb") as image_file:
+            image_file.write(request.data)
+
+        if profile.avator and profile.avator != "default.jpg":
+            pre_file_path = os.join("images/avator/", profile.avator)
+            if default_storage.exists(pre_file_path):
+                default_storage.delete(pre_file_path)
+
+        return file_path
