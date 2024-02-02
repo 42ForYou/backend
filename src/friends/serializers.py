599c3c3ca2e4bd52a227ac41b536ffd643c51fd7
idealflower-k <aekrcu@naver.com>
2024-02-02 19:39:25 +0900

    FY-112/feat: Friend serializer, create, list view

--- src/friends/serializers.py
+++ src/friends/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from .models import Friend
+from accounts.models import User, Profile
+from accounts.serializers import ProfileSerializer
+
+
+class FriendUserSerializer(serializers.ModelSerializer):
+    nickname = serializers.CharField(source="profile.nickname")
+    avatar = serializers.ImageField(source="profile.avatar")
+
+    class Meta:
+        model = User
+        fields = ["nickname", "avatar"]
 
 
 class FriendSerializer(serializers.ModelSerializer):
+    requester = FriendUserSerializer(source="requester", read_only=True)
+    receiver = FriendUserSerializer(source="receiver", read_only=True)
+
     class Meta:
         model = Friend
-        fields = "__all__"
+        fields = ["id", "requester", "receiver", "status", "created_at"]
