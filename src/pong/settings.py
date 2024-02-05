2250ba2c13b6c9cf675fc90898813c98e670ebac
idealflower-k <aekrcu@naver.com>
2024-02-05 14:23:22 +0900

    feat: use env for DB HOST

--- src/pong/settings.py
+++ src/pong/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_NAME"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": 5432,
     }
 }
