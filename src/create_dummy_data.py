fe5d982a5c414a63dc646fd555170c3c3a2c78c0
idealflower-k <aekrcu@naver.com>
2024-02-07 13:15:39 +0900

    feat: create dummy game, gmae_room, game_player

--- src/create_dummy_data.py
+++ src/create_dummy_data.py
@@ -6,9 +6,31 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pong.settings")
 django.setup()
 
 from accounts.models import User, Profile
+from game.models import Game, GameRoom, GamePlayer
 
 # 정해진 첫 번째 이름의 리스트를 정의합니다.
-first_names = ["James", "John", "Robert", "Michael", "William"]
+first_names = [
+    "James",
+    "Olivia",
+    "Michael",
+    "Emma",
+    "Robert",
+    "Ava",
+    "John",
+    "Sophia",
+    "David",
+    "Isabella",
+    "Chris",
+    "Mia",
+    "Daniel",
+    "Charlotte",
+    "Joseph",
+    "Amelia",
+    "William",
+    "Evelyn",
+    "Matthew",
+    "Harper",
+]
 
 
 def create_dummy_users(num_users=10):
@@ -32,14 +54,49 @@ def create_dummy_users(num_users=10):
             user=user,
             nickname=nickname,
             email=email,
-            avatar=f"avatar_{intra_id}.jpg",
+            avatar=f"default.jpg",
             two_factor_auth=False,  # 예시를 단순화하기 위해 항상 False로 설정
+        )
+
+
+def create_game_room(num_games=10):
+    users = User.objects.all()
+
+    for i in range(len(users)):
+        if users[i].username == "admin":
+            continue
+        is_tournament = True
+        if i % 2 == 0:
+            is_tournament = False
+        game = Game.objects.create(
+            is_tournament=is_tournament,
+            game_point=5,
+            time_limit=180,
+            n_players=4,
+        )
+
+        game_room = GameRoom.objects.create(
+            host=users[i],
+            game=game,
+            title=f"GameRoom {i}",
+            is_playing=False,
+            join_players=1,
+        )
+
+        profile = Profile.objects.get(user=users[i])
+
+        player = GamePlayer.objects.create(
+            user=users[i],
+            game=game,
+            nickname=profile.nickname,
+            rank=0,
         )
 
 
 if __name__ == "__main__":
     print("Creating dummy users and profiles...")
-    create_dummy_users(5)  # 사용자와 프로필 생성을 위한 수를 조정할 수 있습니다.
+    create_dummy_users(20)  # 사용자와 프로필 생성을 위한 수를 조정할 수 있습니다.
+    create_game_room(20)
     for i, user in enumerate(User.objects.all().order_by("intra_id")):
         print(f"{i}. {user.username}")
     print("Dummy users and profiles created successfully!")
