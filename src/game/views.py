07aad025e95198d137745df11c165f1ba2928ab7
idealflower-k <aekrcu@naver.com>
2024-02-08 13:51:58 +0900

    FY-97/feat: add order_by for list, change post Player request json

--- src/game/views.py
+++ src/game/views.py
@@ -95,9 +95,11 @@ class GameRoomViewSet(
                 is_tournament = False
                 if filter == "tournament":
                     is_tournament = True
-                game_rooms = GameRoom.objects.filter(game__is_tournament=is_tournament)
+                game_rooms = GameRoom.objects.filter(
+                    game__is_tournament=is_tournament
+                ).order_by("id")
             else:
-                game_rooms = GameRoom.objects.all()
+                game_rooms = GameRoom.objects.all().order_by("id")
             context = paginator.paginate_queryset(game_rooms, request)
             data = []
             for game_room in context:
@@ -223,7 +225,7 @@ class PlayerViewSet(
                     "game_id query string is required",
                     status_code=status.HTTP_400_BAD_REQUEST,
                 )
-            players = GamePlayer.objects.filter(game=game_id)
+            players = GamePlayer.objects.filter(game=game_id).order_by("id")
             serializer = GamePlayerSerializer(players, many=True)
             return Response(
                 wrap_data(players=serializer.data), status=status.HTTP_200_OK
@@ -244,7 +246,7 @@ class PlayerViewSet(
 
     def create(self, request, *args, **kwargs):
         try:
-            game_id = request.data.get("game_id")
+            game_id = request.data.get("data").get("game_id")
             game = Game.objects.get(game_id=game_id)
             game_room = game.game_room
             if not game_id:
@@ -269,7 +271,7 @@ class PlayerViewSet(
             serializer.save()
             game_room.join_players += 1
             game_room.save()
-            players = game.game_player.all()
+            players = game.game_player.all().order_by("id")
             players_serializer = GamePlayerSerializer(players, many=True)
             game_serializer = GameSerializer(game)
             game_room_serializer = GameRoomSerializer(game_room)
