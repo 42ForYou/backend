88f7a9081e0c2bf2cbec38af373f9cac342c873b
idealflower-k <aekrcu@naver.com>
2024-02-13 18:14:40 +0900

    feat: new create_dummy_user.py

--- src/create_dummy_data.py
+++ src/create_dummy_data.py
@@ -1,7 +1,6 @@
 import os
 import django
 
-# Django 설정을 스크립트에서 사용하기 위해 환경 설정
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pong.settings")
 django.setup()
 
@@ -10,64 +9,16 @@ from game.models import Game, GameRoom, GamePlayer
 from friends.models import Friend
 from pong.settings import DEFAULT_AVATAR
 
-# 정해진 첫 번째 이름의 리스트를 정의합니다.
-first_names = [
-    "James",
-    "Olivia",
-    "Michael",
-    "Emma",
-    "Robert",
-    "Ava",
-    "John",
-    "Sophia",
-    "David",
-    "Isabella",
-    "Chris",
-    "Mia",
-    "Daniel",
-    "Charlotte",
-    "Joseph",
-    "Amelia",
-    "William",
-    "Evelyn",
-    "Matthew",
-    "Harper",
-    "Benjamin",
-    "Liam",
-    "Samuel",
-    "Lucas",
-    "Henry",
-    "Owen",
-    "Dylan",
-    "Gabriel",
-    "Aaron",
-    "Elijah",
-    "Nora",
-    "Scarlett",
-    "Zoe",
-    "Lily",
-    "Grace",
-    "Victoria",
-    "Riley",
-    "Madison",
-    "Leah",
-    "Hazel",
-]
 
-
-def create_dummy_users(num_users=10):
-    for i in range(num_users):
-        # 이름 배열의 크기로 순서를 반복합니다.
-        name_index = i % len(first_names)  # 배열의 길이로 나눈 나머지를 사용
-        first_name = first_names[name_index]
-        intra_id = f"{first_name.lower()}"
-
+def create_dummy_users(num_users=100):
+    for i in range(1, num_users + 1):
+        intra_id = "intra_" + str(i)  # 사용자 이름을 숫자로 설정
         email = f"{intra_id}@example.com"
         nickname = f"nick_{intra_id}"
 
         user = User.objects.create_user(
             intra_id=intra_id,
-            username=intra_id,  # 여기서는 username도 intra_id와 동일하게 설정합니다.
+            username=intra_id,
             email=email,
             password="password",
         )
@@ -77,40 +28,29 @@ def create_dummy_users(num_users=10):
             nickname=nickname,
             email=email,
             avatar=DEFAULT_AVATAR,
-            two_factor_auth=False,  # 예시를 단순화하기 위해 항상 False로 설정
+            two_factor_auth=False,
         )
 
 
 def create_friends():
-    users = User.objects.all()
+    users = User.objects.exclude(username="admin")
 
     for i in range(len(users)):
-        if users[i].username == "admin":
-            continue
         for j in range(i + 1, len(users)):
-            if users[j].username == "admin":
-                continue
-            status = "pending"
-            if (j + i) % 2 == 0:
-                status = "friend"
-            friend = Friend.objects.create(
+            status = "friend" if (i + j) % 2 == 0 else "pending"
+            Friend.objects.create(
                 requester=users[i],
                 receiver=users[j],
                 status=status,
             )
 
 
-def create_game_room(num_games=10):
-    users = User.objects.all()
+def create_game_room(num_games=50):
+    users = User.objects.exclude(username="admin")[:50]  # 1부터 30까지의 사용자만 선택
 
-    for i in range(len(users)):
-        if users[i].username == "admin":
-            continue
-        is_tournament = True
-        n_players = 4
-        if i % 2 == 0:
-            is_tournament = False
-            n_players = 2
+    for i, user in enumerate(users):
+        is_tournament = True if (i + 1) % 3 == 0 else False
+        n_players = 4 if is_tournament else 2
         game = Game.objects.create(
             is_tournament=is_tournament,
             game_point=5,
@@ -118,29 +58,73 @@ def create_game_room(num_games=10):
             n_players=n_players,
         )
 
-        game_room = GameRoom.objects.create(
-            host=users[i],
+        GameRoom.objects.create(
+            host=user,
             game=game,
-            title=f"GameRoom {i}",
+            title=f"GameRoom {i + 1}",
             is_playing=False,
             join_players=1,
         )
 
-        profile = Profile.objects.get(user=users[i])
-
-        player = GamePlayer.objects.create(
-            user=users[i],
+        profile = Profile.objects.get(user=user)
+        GamePlayer.objects.create(
+            user=user,
             game=game,
             nickname=profile.nickname,
             rank=0,
         )
 
 
+import random
+
+
+def assign_remaining_users_to_games():
+    # 남은 50명의 사용자를 가져옵니다.
+    remaining_users = User.objects.exclude(username="admin").order_by("-intra_id")[:50]
+
+    # 이미 생성된 50개의 게임을 가져옵니다.
+    games = Game.objects.all()[:50]
+
+    # 게임별로 현재 참가자 수를 추적하기 위한 딕셔너리
+    game_players_count = {
+        game.game_id: GamePlayer.objects.filter(game=game).count() for game in games
+    }
+
+    # 각 게임에 대해 랜덤한 최대 참가자 수 설정 (is_tournament인 경우)
+    max_players_for_tournament_games = {
+        game.game_id: (
+            random.randint(1, game.n_players) if game.is_tournament else game.n_players
+        )
+        for game in games
+    }
+
+    # 남은 사용자를 순회하며 적절한 게임에 할당
+    for user in remaining_users:
+        for game in games:
+            # 현재 게임의 참가자 수가 설정된 랜덤 최대 참가자 수를 넘지 않는 경우에만 추가
+            if (
+                game_players_count[game.game_id]
+                < max_players_for_tournament_games[game.game_id]
+            ):
+                profile = Profile.objects.get(user=user)
+                GamePlayer.objects.create(
+                    user=user,
+                    game=game,
+                    nickname=profile.nickname,
+                    rank=0,
+                )
+                game_room = GameRoom.objects.get(game=game)
+                game_room.join_players += 1
+                game_room.save()
+                # 게임의 참가자 수 업데이트
+                game_players_count[game.game_id] += 1
+                break  # 현재 사용자에 대한 적절한 게임을 찾았으므로 다음 사용자로 넘어감
+
+
 if __name__ == "__main__":
     print("Creating dummy users and profiles...")
-    create_dummy_users(40)  # 사용자와 프로필 생성을 위한 수를 조정할 수 있습니다.
+    create_dummy_users()  # 기본값으로 100명의 사용자 생성
     create_friends()
-    create_game_room(40)
-    for i, user in enumerate(User.objects.all().order_by("intra_id")):
-        print(f"{i}. {user.username}")
+    create_game_room()  # 기본값으로 30개의 게임룸 생성
+    assign_remaining_users_to_games()
     print("Dummy users and profiles created successfully!")
