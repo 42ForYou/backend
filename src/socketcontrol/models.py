e522ad29421bb7bf3300d545ef7c9bfd12c3e75d
idealflower-k <aekrcu@naver.com>
2024-02-14 20:11:39 +0900

    FY-161/feat: create SocketSession model

--- src/socketcontrol/models.py
+++ src/socketcontrol/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class SocketSession(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="socket_session"
+    )
+    session_id = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SocketSession {self.session_id} of {self.user.username}"
