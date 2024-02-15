2d2e9df369fb2e05bc7cb1af51c90961c1f11606
이동현 <earthicko@gmail.com>
2024-02-15 14:29:10 +0900

    Fy 19/feature/사용자는 pong게임을 할 수 있다 (#50)
    * FY-19/feat: add basic GameSession class without method impl
    
    * FY-19/feat: add BallTrackSegment and its logic
    
    * FY-19/feat: improve BallTrackSegment
    
    - calculate len at __init__
    - add method next_dx_dy
    
    * FY-19/feat: improve GameSession.update_paddles
    
    * FY-19/feat: implement BallTrack.calculate_segments
    
    * FY-19/feat: implement __str__ for GameSession classes
    
    * FY-19/feat: implement GameSession logic
    
    * FY-19/feat: rename GameField -> GameConfig & add paddle speed param
    
    * FY-19/fix: remove @property from method requiring params
    
    * FY-19/refactor: separate class definition files
    
    * FY-19/feat: implement epsilon based float comparison
    
    - GameConfig stores epsilon value
    - added flt_eq to GameConfig
    - replaced all == operators with flt_eq
    
    * FY-19/fix: inverted min-max parameters
    
    * FY-19/feat: use Enum at storing heading of the ball
    
    * FY-19/feat: revise BallTrackSegment
    
    - store category of start, end points using Enum
    - don't calculate validity every time, calculate it using calculate_validity()
    - when checking start_from or end_at, store the start/end points' category
    
    * FY-19/fix: select target coordinate based on vector (dx, dy sign)
    
    * FY-19/feat: implement keypress mechanism
    
    * FY-19/fix: bug with paddle impact time
    
    * FY-19/fix: invalid member variable accessor
    
    * FY-19/refactor: __str__ improvement
    
    * FY-19/fix: coordinate of impact sign error
    
    * FY-19/feat: improve __str__ methods
    
    * FY-19/feat: implement GameSession.update_turns()
    
    * FY-19/feat: check dx is not zero when creating GameSession
    
    * FY-19/feat: change paddle update logic
    
    * FY-19/feat: add debug prints

---
+++ src/livegame/BallTrack.py
@@ -0,0 +1,86 @@
+import math
+from typing import List, Tuple
+from enum import Enum
+
+from GameConfig import GameConfig
+from BallTrackSegment import (
+    BallTrackSegment,
+    get_ball_track_segment_to_wall,
+    get_ball_track_segment_to_paddle,
+)
+
+
+class BallTrack:
+    class Heading(Enum):
+        LEFT = 1
+        RIGHT = 2
+
+    def __init__(
+        self,
+        config: GameConfig,
+        x_start: float,
+        y_start: float,
+        dx_start: float,
+        dy_start: float,
+        t_start: float,
+    ) -> None:
+        if config.flt_eq(dx_start, 0.0):
+            raise ValueError("Cannot construct BallTrack because dx is 0.0")
+        self.config = config
+        self.t_start = t_start
+        self.v = math.hypot(dx_start, dy_start)
+        self.heading = (
+            BallTrack.Heading.LEFT if dx_start < 0 else BallTrack.Heading.RIGHT
+        )
+        # below: variables should be calculated with calculate_segments()
+        self.segments: List[BallTrackSegment] = []
+        self.y_impact: float = 0.0
+        self.t_duration: float = 0.0
+        self.t_end: float = 0.0
+        self.calculate_segments(x_start, y_start, dx_start, dy_start)
+
+    # Calculates the all ball track segments based on initial (x, y) and (dx, dy)
+    # until the ball gets out of the game
+    def calculate_segments(
+        self, x_start: float, y_start: float, dx: float, dy: float
+    ) -> None:
+        while True:
+            next_track = get_ball_track_segment_to_wall(
+                self.config, x_start, y_start, dx, dy
+            )
+            if next_track.is_valid:
+                self.segments.append(next_track)
+                x_start = next_track.x_end
+                y_start = next_track.y_end
+                dx, dy = next_track.next_dx_dy
+                continue
+
+            # ball doesn't hit the wall, thus must hit paddle side
+            next_track = get_ball_track_segment_to_paddle(
+                self.config, x_start, y_start, dx, dy
+            )
+            if next_track.is_valid:
+                self.segments.append(next_track)
+                break
+
+            # ball doesn't hit walls neither paddles?
+            raise ValueError("Error while calculating ball's movement")
+
+        self.y_impact = self.segments[-1].y_end
+        len_total = sum([seg.len for seg in self.segments])
+        self.t_duration = len_total / self.v  # v * t = d, t = d / v
+        self.t_end = self.t_start + self.t_duration
+
+    @property
+    def next_dx_dy(self) -> Tuple[float, float]:
+        return self.segments[-1].next_dx_dy
+
+    @property
+    def next_xy_start(self) -> Tuple[float, float]:
+        return self.segments[-1].next_xy_start
+
+    def __str__(self) -> str:
+        pts = [f"({seg.x_start}, {seg.y_start})" for seg in self.segments]
+        pts.append(f"({self.segments[-1].x_end}, {self.segments[-1].y_end})")
+        pts_str = " > ".join(pts)
+        return f"BallTrack {self.heading.name}, dt={self.t_duration}, t={self.t_start}...{self.t_end}, v={self.v}, {pts_str}"
---
+++ src/livegame/BallTrackSegment.py
@@ -0,0 +1,210 @@
+import math
+from typing import Tuple
+from enum import Enum
+
+from GameConfig import GameConfig
+
+
+class BallTrackSegment:
+    class PointCategory(Enum):
+        INVALID = 0
+        CENTER = 1
+        WALL_TOP = 2
+        WALL_BOTTOM = 3
+        PADDLE_LEFT = 4
+        PADDLE_RIGHT = 5
+
+    def __init__(
+        self,
+        config: GameConfig,
+        x_start: float,
+        y_start: float,
+        x_end: float,
+        y_end: float,
+        dx: float,
+        dy: float,
+    ) -> None:
+        self.config = config
+        self.x_start = x_start
+        self.y_start = y_start
+        self.x_end = x_end
+        self.y_end = y_end
+        self.dx = dx
+        self.dy = dy
+        self.len = math.hypot(self.x_end - self.x_start, self.y_end - self.y_start)
+        self.p_start = BallTrackSegment.PointCategory.INVALID
+        self.p_end = BallTrackSegment.PointCategory.INVALID
+        self.is_valid = False
+        self.calculate_validity()
+
+    def __str__(self) -> str:
+        result = f"{self.__class__.__name__} "
+        result += f"s=({self.x_start}, {self.y_start}) [{self.p_start.name}] -> "
+        result += f"e=({self.x_end}, {self.y_end}) [{self.p_end.name}], "
+        result += f"v=({self.dx}, {self.dy}), l={self.len}"
+        return result
+
+    @property
+    def start_from_center(self) -> bool:
+        result = self.config.flt_eq(self.x_start, 0.0) and self.config.flt_eq(
+            self.y_start, 0.0
+        )
+        if result:
+            self.p_start = BallTrackSegment.PointCategory.CENTER
+        return result
+
+    def is_inside_line(
+        self,
+        p_coord_constr: float,
+        l_constr_min: float,
+        l_constr_max: float,
+        p_coord_range: float,
+        l_range_min: float,
+        l_range_max: float,
+    ) -> bool:
+        if not (
+            self.config.flt_eq(p_coord_constr, l_constr_min)
+            or self.config.flt_eq(p_coord_constr, l_constr_max)
+        ):
+            return False
+        if not (l_range_min <= p_coord_range <= l_range_max):
+            return False
+        return True
+
+    @property
+    def start_from_walls(self) -> bool:
+        result = self.is_inside_line(
+            self.y_start,
+            self.config.y_min,
+            self.config.y_max,
+            self.x_start,
+            self.config.x_min,
+            self.config.x_max,
+        )
+        if result:
+            if self.y_start > 0:
+                self.p_start = BallTrackSegment.PointCategory.WALL_TOP
+            else:
+                self.p_start = BallTrackSegment.PointCategory.WALL_BOTTOM
+        return result
+
+    @property
+    def end_at_walls(self) -> bool:
+        result = self.is_inside_line(
+            self.y_end,
+            self.config.y_min,
+            self.config.y_max,
+            self.x_end,
+            self.config.x_min,
+            self.config.x_max,
+        )
+        if result:
+            if self.y_end > 0:
+                self.p_end = BallTrackSegment.PointCategory.WALL_TOP
+            else:
+                self.p_end = BallTrackSegment.PointCategory.WALL_BOTTOM
+        return result
+
+    @property
+    def start_from_paddles(self) -> bool:
+        result = self.is_inside_line(
+            self.x_start,
+            self.config.x_min,
+            self.config.x_max,
+            self.y_start,
+            self.config.y_min,
+            self.config.y_max,
+        )
+        if result:
+            if self.x_start > 0:
+                self.p_start = BallTrackSegment.PointCategory.PADDLE_RIGHT
+            else:
+                self.p_start = BallTrackSegment.PointCategory.PADDLE_LEFT
+        return result
+
+    @property
+    def end_at_paddles(self) -> bool:
+        result = self.is_inside_line(
+            self.x_end,
+            self.config.x_min,
+            self.config.x_max,
+            self.y_end,
+            self.config.y_min,
+            self.config.y_max,
+        )
+        if result:
+            if self.x_end > 0:
+                self.p_end = BallTrackSegment.PointCategory.PADDLE_RIGHT
+            else:
+                self.p_end = BallTrackSegment.PointCategory.PADDLE_LEFT
+        return result
+
+    @property
+    def next_dx_dy(self) -> Tuple[float, float]:
+        if self.end_at_walls:  # vertical reflection
+            return (self.dx, -self.dy)
+        if self.end_at_paddles:  # horizontal reflection
+            return (-self.dx, self.dy)
+        raise ValueError("BallTrackSegment doesn't end at either walls or paddles")
+
+    @property
+    def next_xy_start(self) -> Tuple[float, float]:
+        return (self.x_end, self.y_end)
+
+    def calculate_validity(self) -> None:
+        self.is_valid = False
+        # start point can be either (0, 0) | on walls | on paddles
+        if not (
+            self.start_from_center or self.start_from_walls or self.start_from_paddles
+        ):
+            return
+        # end point can be either on walls | on paddles
+        if not (self.end_at_walls or self.end_at_paddles):
+            return
+
+        if (
+            self.p_start == BallTrackSegment.PointCategory.INVALID
+            or self.p_end == BallTrackSegment.PointCategory.INVALID
+        ):
+            raise ValueError(f"p_start or p_end is invalid")
+        self.is_valid = True
+
+
+def get_ball_track_segment_to_wall(
+    config: GameConfig,
+    x_start: float,
+    y_start: float,
+    dx: float,
+    dy: float,
+) -> BallTrackSegment:
+    if config.flt_eq(dy, 0.0):
+        raise ValueError("Ball cannot reach the walls (dy == 0.0)")
+
+    y_impact = config.y_max if dy > 0 else config.y_min
+    # Calculate time to reach the wall (y = y_impact | -y_impact)
+    t_impact = (y_impact - y_start) / dy
+    # Calculate the x-coordinate of the impact point
+    x_impact = x_start + dx * t_impact
+    # Determine which wall is hit
+
+    return BallTrackSegment(config, x_start, y_start, x_impact, y_impact, dx, dy)
+
+
+def get_ball_track_segment_to_paddle(
+    config: GameConfig,
+    x_start: float,
+    y_start: float,
+    dx: float,
+    dy: float,
+) -> BallTrackSegment:
+    if config.flt_eq(dx, 0.0):
+        raise ValueError("Ball cannot reach the paddles (dx == 0.0)")
+
+    x_impact = config.x_max if dx > 0 else config.x_min
+    # Calculate time to reach the paddles (x = x_impact | -x_impact)
+    t_impact = (x_impact - x_start) / dx
+    # Calculate the y-coordinate of the impact point
+    y_impact = y_start + dy * t_impact
+    # Determine which paddle is hit
+
+    return BallTrackSegment(config, x_start, y_start, x_impact, y_impact, dx, dy)
