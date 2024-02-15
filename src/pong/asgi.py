ae5880eb5f6257c91961f4e6f2cc42a027618ef1
Donghyeon Lee <earthicko@gmail.com>
2024-02-15 20:36:52 +0900

    FY-166/refactor: move sio object to separate file and improve handler registering

--- src/pong/asgi.py
+++ src/pong/asgi.py
@@ -15,18 +15,9 @@ from livegame.events import register_sio_game
 
 
 django_asgi_app = get_asgi_application()
-sio = socketio.AsyncServer(
-    async_mode="asgi",
-    cors_allowed_origins=["https://localhost", "https://localhost:8000"],
-    logger=True,
-    engineio_logger=True,
+
+socketio_app = socketio.ASGIApp(
+    sio, django_asgi_app, on_startup=update_game_session_registry_forever()
 )
-
-register_sio_control(sio)
-register_sio_chat(sio)
-register_sio_game(sio)
-
-socketio_app = socketio.ASGIApp(sio, django_asgi_app)
-
 
 application = socketio_app
