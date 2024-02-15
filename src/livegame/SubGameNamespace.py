ed41c3efd37eee3ae6d63d0f318b0607e430fccc
Donghyeon Lee <earthicko@gmail.com>
2024-02-15 15:00:32 +0900

    FY-166/fix: event handler가 아닌 함수들을 Namespace 클래스에서 분리

--- src/livegame/SubGameNamespace.py
+++ src/livegame/SubGameNamespace.py
@@ -1,6 +1,30 @@
 import socketio
 
 
+def emit_start():
+    pass
+
+
+def emit_update_time_left():
+    pass
+
+
+def emit_ended():
+    pass
+
+
+def emit_update_scores():
+    pass
+
+
+def emit_update_track_ball():
+    pass
+
+
+def emit_update_track_paddle():
+    pass
+
+
 class SubGameNamespace(socketio.Namespace):
     def on_connect(self, sid, environ):
         pass
@@ -8,34 +32,10 @@ class SubGameNamespace(socketio.Namespace):
     def on_disconnect(self, sid):
         pass
 
-    def on_start(self, sid, data):
-        # self.emit("my_response", data)
-        pass
-
-    def on_update_time_left(self, sid, data):
-        # self.emit("my_response", data)
-        pass
-
-    def on_ended(self, sid, data):
-        # self.emit("my_response", data)
-        pass
-
     def on_leave(self, sid, data):
         # self.emit("my_response", data)
         pass
 
-    def on_update_scores(self, sid, data):
-        # self.emit("my_response", data)
-        pass
-
-    def on_update_track_ball(self, sid, data):
-        # self.emit("my_response", data)
-        pass
-
     def on_keyboard_input(self, sid, data):
-        # self.emit("my_response", data)
-        pass
-
-    def on_update_track_paddle(self, sid, data):
         # self.emit("my_response", data)
         pass
