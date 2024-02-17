8f197452087985f62834c543d5d96825c85959e1
earthicko <earthicko@gmail.com>
2024-02-18 01:41:15 +0900

    FY-166/feat: add init point and ball init coord, remove ball init dx,dy from SubGameConfig

--- src/livegame/SubGameConfig.py
+++ src/livegame/SubGameConfig.py
@@ -6,17 +6,22 @@ class SubGameConfig:
         self,
         width: float,
         height: float,
-        match_point: float,
+        match_point: int,
+        player_a_init_point: int,
+        player_b_init_point: int,
         paddle_len: float,
         paddle_speed: float,
         epsilon: float,  # expected floating point error
-        dx_ball: float,
-        dy_ball: float,
+        ball_init_x: float,
+        ball_init_y: float,
+        ball_speed: float,
         time_limit: float,
     ) -> None:
         self.width = width
         self.height = height
         self.match_point = match_point
+        self.player_a_init_point = player_a_init_point
+        self.player_b_init_point = player_b_init_point
         self.x_max = width / 2
         self.x_min = -width / 2
         self.y_max = height / 2
@@ -24,9 +29,9 @@ class SubGameConfig:
         self.l_paddle = paddle_len
         self.v_paddle = paddle_speed
         self.e = abs(epsilon)
-        self.dx_ball = dx_ball
-        self.dy_ball = dy_ball
-        self.v_ball = math.hypot(dx_ball, dy_ball)
+        self.x_ball_init = ball_init_x
+        self.y_ball_init = ball_init_y
+        self.v_ball = ball_speed
         self.t_limit = time_limit
 
     def __str__(self) -> str:
