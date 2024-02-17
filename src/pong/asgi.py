056bfdac861833bc92c100aaba4b03e2d48c6f15
earthicko <earthicko@gmail.com>
2024-02-17 13:37:21 +0900

    FY-166/revert: remove polling updater

--- src/pong/asgi.py
+++ src/pong/asgi.py
@@ -9,15 +9,10 @@ django.setup()
 
 from django.core.asgi import get_asgi_application
 from socketcontrol.events import sio
-from livegame.GameSessionRegistry import update_game_session_registry_forever
 from friends.OnlineStatusNamespace import OnlineStatusNamespace
 
 
 django_asgi_app = get_asgi_application()
-
-
-# async def on_startup():
-#     asyncio.create_task(update_game_session_registry_forever())
 
 
 socketio_app = socketio.ASGIApp(sio, django_asgi_app)
