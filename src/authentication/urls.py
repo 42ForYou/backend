4420c308ed78d594deb11bdc8887068d6b800c71
idealflower-k <aekrcu@naver.com>
2024-02-25 19:57:21 +0900

    FY-138/feat: add urls for jwt

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import LoginView, LogoutView, TokenValidationView, TwoFactorAuthView
+from rest_framework_simplejwt.views import TokenVerifyView
+from .views import (
+    LoginView,
+    LogoutView,
+    TokenValidationView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView,
+    TwoFactorAuthView,
+)
 from .oauthviews import OAuthView
 
 
@@ -8,5 +16,7 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("oauth/", OAuthView.as_view(), name="oauth"),
     path("valid/", TokenValidationView.as_view(), name="valid"),
+    path("token/verify/", CustomTokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("2fa/", TwoFactorAuthView.as_view(), name="2fa"),
 ]
