3deadfdd8287746bb39339b03c110e6a1d6d48b0
idealflower-k <aekrcu@naver.com>
2024-02-26 16:45:28 +0900

    FY-138/feat: make util func for response data when login

--- src/authentication/utils.py
+++ src/authentication/utils.py
@@ -1,4 +1,6 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+from accounts.serializers import UserSerializer, ProfileSerializer
+from pong.utils import wrap_data
 import pong.settings as settings
 
 
@@ -28,3 +30,10 @@ def set_cookie_response(response, access=None, refresh=None):
             path="/",
         )
     return response
+
+
+def get_response_data(user):
+    user_serializer = UserSerializer(user).data
+    profile = ProfileSerializer(user.profile).data
+    user_serializer.update(profile)
+    return wrap_data(user=user_serializer)
