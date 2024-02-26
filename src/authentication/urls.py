56f0c5373ab056e7079234e533aceea915f6941a
idealflower-k <aekrcu@naver.com>
2024-02-26 16:44:51 +0900

    FY-138/chore: remove /api/valid endpoint

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenVerifyView
 from .views import (
     LoginView,
     LogoutView,
-    TokenValidationView,
     CustomTokenRefreshView,
     CustomTokenVerifyView,
     TwoFactorAuthView,
@@ -15,7 +13,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("oauth/", OAuthView.as_view(), name="oauth"),
-    path("valid/", TokenValidationView.as_view(), name="valid"),
     path("token/verify/", CustomTokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("2fa/", TwoFactorAuthView.as_view(), name="2fa"),
