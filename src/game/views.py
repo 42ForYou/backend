2ddfa2b41dffe14333729b08b46b0581e871ef29
earthicko <earthicko@gmail.com>
2024-03-02 22:42:39 +0900

    FY-195/chore: pylint: arguments-differ

--- src/game/views.py
+++ src/game/views.py
@@ -59,7 +59,7 @@ class GameRoomViewSet(
     permission_classes = [permissions.IsAuthenticated, IsPlayerInGameRoom]
     pagination_class = CustomPageNumberPagination
 
-    def list(self, request):
+    def list(self, request, *_, **__):
         try:
             paginator = CustomPageNumberPagination()
             filter_val = request.query_params.get("filter", None)
@@ -182,7 +182,7 @@ class PlayerViewSet(
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [CookieTokenAuthentication]
 
-    def list(self, request, game_id=None):
+    def list(self, request, game_id=None, *_, **__):
         try:
             game_id = request.query_params.get("game_id", None)
             if not game_id:
