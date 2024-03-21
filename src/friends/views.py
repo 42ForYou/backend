8c04863469b65d1aad6ae45cf50146ee55feb882
idealflower-k <aekrcu@naver.com>
2024-03-21 21:16:42 +0900

    chore: remove unused import lib in friends

--- src/friends/views.py
+++ src/friends/views.py
@@ -1,14 +1,16 @@
+from django.db.models import Q
+
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
 
+from accounts.models import Profile
 from .models import Friend
 from .serializers import *
 
 from pong.utils import CustomError, CookieTokenAuthentication
-from django.db.models import Q
 from pong.utils import CustomPageNumberPagination
 
 
