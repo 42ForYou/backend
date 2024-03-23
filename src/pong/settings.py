401a3e32b4cdb94516bb9c250782c10da31d6fc5
earthicko <earthicko@gmail.com>
2024-03-23 23:07:13 +0900

    FY-195/fix: settings에서 rest_framework.authtoken

--- src/pong/settings.py
+++ src/pong/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
     "rest_framework_simplejwt",
     'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
