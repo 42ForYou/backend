a32070a1d2f977b5b658ff89b9f1b20cb0c0572f
earthicko <earthicko@gmail.com>
2024-03-23 23:55:08 +0900

    FY-195/build: move secret key to .env

--- src/pong/settings.py
+++ src/pong/settings.py
@@ -26,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# TODO: move to .env
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--tx=7@tvtt)n%fb-l*l-mp-xf(60x9o)&09z2lu%@@5#e-0ys0"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "") != ""
